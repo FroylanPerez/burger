57d62ae7bf7a7fb20c95adf6c430bb6d0a8fc3d2
Froylan Perez <fperez@noryan.com.mx>
2019-12-06 20:00:07 -0600

    Creacion de carpetas, archivos iniciales y base de datos

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Tyrano Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pumpkin Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shrimp Burger', true);
